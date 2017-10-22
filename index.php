37ed03812aaf3629f7acde560dbe2e3a9de72297
Sylvester Yoo <yoostack@cryptolab.net>
2017-10-22 16:40:02 +0800

    basic skeleton for a website

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS Files-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Blue Hacks 2018</title>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div id="main-container">
+        
+        <nav>
+            <div class="brand-logo">
+                <img src="./images/logo.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#history">History</a></li>
+                    <li><a href="#signup">Signup</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div id="intro">
+            <h1>Blue Hacks 2018 is Coming Soon</h1>
+            <p class="code">
+                Welcome to Blue Hacks 2018.
+                Please input your idea:
+            </p>
+            <button type="button" onclick="alert('Signup')"> Click to Sign up </button>
+        </div>
+
+        <div id="about">
+        </div>
+
+        <div id="history">
+            <div class="info">
+            <!-- Some Information -->
+            </div>
+
+            <div class="gallery">
+            <!-- Some Gallery stuff, I suppose -->
+            </div>
+        </div>
+
+        <div id="sponsors">
+            
+        </div>
+
+        <div id="signup">
+
+        </div>    
+    </div>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+
+</body>
+</html>
